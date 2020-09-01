7a041c21a9fb188648f78b6bdb41a24d1b475ee6
Josh Ellis <josh@imjoshellis.com>
2020-09-01 15:09:56 -0500

    Add spinner

--- src/layouts/SpinnerPage.tsx
+++ src/layouts/SpinnerPage.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Spinner } from '../components'
 
 interface SpinnerPageProps {}
 
 export const SpinnerPage: React.FC<SpinnerPageProps> = () => {
-  return <></>
+  return (
+    <>
+      <Spinner
+        on={true}
+        setOn={() => null}
+        startPlayer={1}
+        playerCount={2}
+        startFn={() => null}
+        lastRotation={0}
+        startRotation={0}
+      />
+      <p className='text-sm font-bold text-gray-700'>(YOU)</p>
+    </>
+  )
 }
 
 export default SpinnerPage
