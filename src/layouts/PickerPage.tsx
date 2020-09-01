1c10ec61022ca46da8a557620af6fc25d7d36220
Josh Ellis <josh@imjoshellis.com>
2020-09-01 15:01:35 -0500

    Create pages

---
+++ src/layouts/PickerPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface PickerPageProps {}
+
+export const PickerPage: React.FC<PickerPageProps> = () => {
+  return <></>
+}
+
+export default PickerPage
---
+++ src/layouts/SpinnerPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface SpinnerPageProps {}
+
+export const SpinnerPage: React.FC<SpinnerPageProps> = () => {
+  return <></>
+}
+
+export default SpinnerPage
