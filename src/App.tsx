1c15215ba8712c04bfc0311f495714a395e1de23
Josh Ellis <josh@imjoshellis.com>
2020-09-01 18:52:36 -0500

    Add transition animations

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import MainLayout from './layouts/MainLayout'
 import PlayerCountContext from './PlayerCountContext'
 import PlayerCountGrid from './PlayerCountGrid'
 import StartPlayerSpinner from './StartPlayerSpinner'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export default function App () {
   const [playerCount, setPlayerCount] = useState(0)
@@ -15,11 +16,28 @@ export default function App () {
     [playerCount, setPlayerCount]
   )
 
+  const motionProps = {
+    initial: { opacity: 0, y: -10, scale: 0.9 },
+    animate: { opacity: 1, y: 0, scale: 1 },
+    exit: { opacity: 0, y: 10, scale: 0.9 },
+    transition: { duration: 0.1, ease: 'easeOut' }
+  }
+
   return (
     <MainLayout>
       <Header />
       <PlayerCountContext.Provider value={providerStartPlayerCount}>
-        {playerCount === 0 ? <PlayerCountGrid /> : <StartPlayerSpinner />}
+        <AnimatePresence exitBeforeEnter>
+          {playerCount === 0 ? (
+            <motion.div {...motionProps} key='grid'>
+              <PlayerCountGrid />
+            </motion.div>
+          ) : (
+            <motion.div {...motionProps} key='spinner'>
+              <StartPlayerSpinner />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </PlayerCountContext.Provider>
     </MainLayout>
   )
