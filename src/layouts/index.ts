a5bcfa66f27df535689832d9c70f6725dd3adbd8
Josh Ellis <josh@imjoshellis.com>
2020-09-01 15:09:29 -0500

    Create index.ts

---
+++ src/layouts/index.ts
@@ -0,0 +1,6 @@
+import Header from './Header'
+import MainLayout from './MainLayout'
+import PickerPage from './PickerPage'
+import SpinnerPage from './SpinnerPage'
+
+export { SpinnerPage, Header, PickerPage, MainLayout }
