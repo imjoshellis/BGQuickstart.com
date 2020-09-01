3c3ae04980b3ec4717719d15a252a41a25be4193
Josh Ellis <josh@imjoshellis.com>
2020-09-01 15:09:48 -0500

    Add player count grid

--- src/layouts/PickerPage.tsx
+++ src/layouts/PickerPage.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import PlayerCountButton from '../components/PlayerCountButton'
+
+const PLAYER_COUNTS = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 interface PickerPageProps {}
 
 export const PickerPage: React.FC<PickerPageProps> = () => {
-  return <></>
+  return (
+    <>
+      <div className='grid grid-cols-3 gap-4'>
+        {PLAYER_COUNTS.map(n => (
+          <PlayerCountButton key={n} num={n} handleClick={() => null} />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default PickerPage
