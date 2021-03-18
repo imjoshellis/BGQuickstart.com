8890f4c374591e50aed69d07d010d879e54c1207
Josh Ellis <josh@imjoshellis.com>
2021-03-17 20:26:41 -0500

    Try new things

--- src/sw.js
+++ src/sw.js
@@ -2,11 +2,6 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js'
 )
 
-workbox.loadModule('workbox-precaching')
-workbox.loadModule('workbox-routing')
-workbox.loadModule('workbox-strategies')
-workbox.loadModule('workbox-expiration')
-
 const { precacheAndRoute } = workbox.precaching
 const { registerRoute } = workbox.routing
 const { StaleWhileRevalidate } = workbox.strategies
