4e44dd3db81fbf02ae9ca6aa691818b7e9b0e317
Josh Ellis <josh@imjoshellis.com>
2021-03-17 20:11:46 -0500

    Go back to no routes

--- src/sw.js
+++ src/sw.js
@@ -13,19 +13,3 @@ const { StaleWhileRevalidate } = workbox.strategies
 const { ExpirationPlugin } = workbox.expiration
 
 precacheAndRoute(self.__WB_MANIFEST)
-
-// An example runtime caching route for requests that aren't handled by the
-// precache, in this case same-origin .png requests like those from in public/
-registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
-  ({ url }) =>
-    url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
-    cacheName: 'images',
-    plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 })
-    ]
-  })
-)
