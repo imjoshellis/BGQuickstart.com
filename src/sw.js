9945837d967c2841e0687efc496672305efb7f2c
Josh Ellis <josh@imjoshellis.com>
2021-03-17 20:01:49 -0500

    Add sw routing

--- src/sw.js
+++ src/sw.js
@@ -3,6 +3,29 @@ importScripts(
 )
 
 workbox.loadModule('workbox-precaching')
+workbox.loadModule('workbox-routing')
+workbox.loadModule('workbox-strategies')
 
-const { precacheAndRoute } = workbox.precaching
-precacheAndRoute(self.__WB_MANIFEST)
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
+
+const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$')
+workbox.routing.registerRoute(
+  // Return false to exempt requests from being fulfilled by index.html.
+  ({ request, url }) => {
+    // If this isn't a navigation, skip.
+    if (request.mode !== 'navigate') {
+      return false
+    } // If this is a URL that starts with /_, skip.
+
+    if (url.pathname.startsWith('/_')) {
+      return false
+    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
+
+    if (url.pathname.match(fileExtensionRegexp)) {
+      return false
+    } // Return true to signal that we want to use the handler.
+
+    return true
+  },
+  new workbox.strategies.StaleWhileRevalidate()
+)
